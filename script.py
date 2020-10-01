4479a4ad80022d9befc6036c6ce8b6d65f987024
AnkitaMalviya <72163210+AnkitaMalviya@users.noreply.github.com>
2020-10-01 12:36:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# Different types of pizzas
 toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
 prices = [2, 6, 1, 3, 2, 7, 2]
 num_pizzas = len(toppings)
@@ -10,8 +11,8 @@ priciest_pizza = pizzas[-1]
 three_cheapest = pizzas[0:3]
 print(three_cheapest)
 num_two_doller_slices = prices.count(2)
-
-
+print("Cheapest pizza is",cheapest_pizza)
+print("Priciest pizza is",priciest_pizza)
 
 
 
